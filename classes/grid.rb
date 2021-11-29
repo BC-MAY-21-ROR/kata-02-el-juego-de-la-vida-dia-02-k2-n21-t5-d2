a6fff612dd3d0713659e9bee5dd7b450e431f5b3
diegoOG09 <xdiegoemmax@outlook.com>
2021-11-29 13:18:46 -0600

    Correction of code (it did not work properly in the output)
    -The $ symbol was omitted in the cells.rb and grid.rb files
     as this caused the live and dead cell count to not work
    
     Co-authored-by: lfrocharod  <lfrocharod01@gmail.com>
     Co-authored-by: Joshsam31 <samano82324@gmail.com>

--- classes/grid.rb
+++ classes/grid.rb
@@ -32,7 +32,7 @@ class Grid
     def dibujar(grid, fil, col)
       for i in(0..fil - 1)
         for j in (0..col - 1)
-          if $grid[i][j] == 1
+          if grid[i][j] == 1
             print('*')
           else
             print('.')
